a5013d10a5ce7319b23534d67dd818efb3559019
dbenj <dbenj@172.19.166.232>
2022-01-27 17:52:39 -0600

    done

--- rubik/check.py
+++ rubik/check.py
@@ -2,111 +2,39 @@ import rubik.cube as rubik
 from collections import Counter
 
 def _eachFaceHasAColor(parms):
-     encodedCube = parms.get('cube',None)    
+        encodedCube = parms.get('cube',None)    
         
 def _check(parms):
     result={}
-    color1 = None
-    color2 = None
-    color3 = None
-    color4 = None
-    color5 = None
-    color6 = None
     
-    countColor1 = None
-    countColor2  = None
-    countColor3 = None
-    countColor4 = None
-    countColor5 = None
-    countColor6 = None
     
     encodedCube = parms.get('cube',None)       
-    isS = 0
     if (parms.get('cube') != None):
-        
-        isS =  isinstance(encodedCube, str)  
-        if (len(encodedCube) <= 54 and len(encodedCube) >= 50):
-            color1 = encodedCube[4]
-            color2 = encodedCube[31]
-            color3 = encodedCube[49]
-            color4 = encodedCube[22]
-            color5 = encodedCube[13]
-            color6 = encodedCube[40] 
-        
-            countColor1 = encodedCube.count(color1)
-            countColor2  = encodedCube.count(color2)
-            countColor3 = encodedCube.count(color3)
-            countColor4 = encodedCube.count(color4)
-            countColor5 = encodedCube.count(color5)
-            countColor6 = encodedCube.count(color6)
-        
-        
-        elif (len(encodedCube) < 50 and len(encodedCube) >= 41):   
-             color1 = encodedCube[4]
-             color2 = encodedCube[31]
-             color4 = encodedCube[22]
-             color5 = encodedCube[13]
-             color6 = encodedCube[40]
-         
-             countColor1 = encodedCube.count(color1)
-             countColor2  = encodedCube.count(color2)
-             countColor4 = encodedCube.count(color4)
-             countColor5 = encodedCube.count(color5)
-             countColor6 = encodedCube.count(color6)
-         
-        elif (len(encodedCube) < 41 and len(encodedCube) >= 32):
-            color1 = encodedCube[4]
-            color2 = encodedCube[31]
-            color4 = encodedCube[22]
-            color5 = encodedCube[13]
-         
-            countColor1 = encodedCube.count(color1)
-            countColor2  = encodedCube.count(color2)
-            countColor4 = encodedCube.count(color4)
-            countColor5 = encodedCube.count(color5)
-         
-        elif (len(encodedCube) < 32 and len(encodedCube) >= 23):
-            color1 = encodedCube[4]
-            color4 = encodedCube[22]
-            color5 = encodedCube[13]
-         
-            countColor1 = encodedCube.count(color1)
-            countColor4 = encodedCube.count(color4)
-            countColor5 = encodedCube.count(color5)
-        elif (len(encodedCube) < 23 and len(encodedCube) >= 14):
-            color1 = encodedCube[4]
-            color5 = encodedCube[13]       
-         
-            countColor1 = encodedCube.count(color1)
-            countColor5 = encodedCube.count(color5)
-         
-        elif (len(encodedCube) < 14 and len(encodedCube) >= 5):
-            color1 = encodedCube[4]    
-   
-            countColor1 = encodedCube.count(color1)
-            
-    else:
-       result['status'] = 'error: cube is missing' 
-       return result
+          
+        if (isinstance(encodedCube, str) == True):
+
      
-    if (encodedCube != None) and (isS == True) and (len(encodedCube) == 54) and (countColor1 == countColor2 == countColor3 == countColor4 == countColor5 == countColor6 == 9) and (color1 != color2 != color3 != color4 != color5 != color6) :
-        result['status'] = 'ok' 
+            if (encodedCube != None) and (len(encodedCube) == 54) and (encodedCube.count(encodedCube[49]) == encodedCube.count(encodedCube[4]) == encodedCube.count(encodedCube[13]) == encodedCube.count(encodedCube[31]) == encodedCube.count(encodedCube[22]) == encodedCube.count(encodedCube[40]) == 9) and (encodedCube[4] != encodedCube[13] != encodedCube[22] != encodedCube[31] != encodedCube[40]!= encodedCube[49]) :
+                result['status'] = 'ok' 
+       
         
+            elif (len(encodedCube) < 54):
         
-    elif (isS == False):
-        result['status'] = 'error: cube is invalid'
-        
-    elif (len(encodedCube) < 54):
-        
-        result['status'] = 'error: cube is too short'
+                result['status'] = 'error: cube is too short'
+                
+            elif (encodedCube.count(encodedCube[4]) != 9 or encodedCube.count(encodedCube[31]) != 9 or encodedCube.count(encodedCube[49]) != 9 or encodedCube.count(encodedCube[22]) != 9 or encodedCube.count(encodedCube[13]) != 9 or encodedCube.count(encodedCube[40]) != 9):
+                result['status'] = 'error: cube must have 9 instances per color'
 
-    elif (color1 == color2 or color1 == color3 or color1 == color4 or color1 == color5 or color1 == color6 or color2 == color3 or color2 == color4 or color2 == color5 or color2 == color6 or color3 == color4 or color3 == color5 or color3 == color6 or color4 == color5 or color4 == color6 or color5 == color6):
-        result['status'] = 'error: cube must have 6 distinct colors'
+            elif (encodedCube[4] == encodedCube[31] or encodedCube[4] == encodedCube[49] or encodedCube[4] == encodedCube[22] or encodedCube[4] == encodedCube[13] or encodedCube[4] == encodedCube[40] or encodedCube[31] == encodedCube[49] or encodedCube[31] == encodedCube[22] or encodedCube[31] == encodedCube[13] or encodedCube[31] == encodedCube[40] or encodedCube[49] == encodedCube[22] or encodedCube[49] == encodedCube[13] or encodedCube[49] == encodedCube[40] or encodedCube[22] == encodedCube[13] or encodedCube[22] == encodedCube[40] or encodedCube[13] == encodedCube[40]):
+                result['status'] = 'error: cube must have each middle face with a different color'
         
-    elif (countColor1 != countColor2 != countColor3 != countColor4 != countColor5 != countColor6 != 9):
-        result['status'] = 'error: cube must have 9 instances per color'
-    return result
+            return result
 
-    
+     
+        else:
+         result['status'] = 'error: cube is invalid'
+         return result
 
-
+    else:
+      result['status'] = 'error: cube is missing'
+      return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -25,14 +25,14 @@ class CheckTest(TestCase):
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
-        self.assertEqual(status, 'ok')
+        self.assertEqual(status, 'error: cube is too short')
         
     def test_check_013_ShouldReturnErrorOnMixedCube(self):
         parm = {'op':'check'}
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
-        self.assertEqual(status, 'ok')
+        self.assertEqual(status, 'error: cube is missing')
                  
     def test_check_014_ShouldReturnErrorOnMixedCube(self):
         parm = {'op':'check',
@@ -40,5 +40,26 @@ class CheckTest(TestCase):
         result = check._check(parm)
         self.assertIn('status', result)
         status = result.get('status', None)
-        self.assertEqual(status, 'ok')
-    
+        self.assertEqual(status, 'error: cube is invalid')
+    def test_check_015_ShouldReturnErrorOnCube(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbbrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must have 9 instances per color')
+        
+    def test_check_016_ShouldReturnErrorOnMixedCube(self):
+        #encodedCube[4] = 1
+        #encodedCube[13] = 1
+        #encodedCube[22] = g
+        #encodedCube[31] = a
+        #encodedCube[40] = y
+        #encodedCube[49] = w
+        parm = {'op':'check',
+                'cube':'1rwr1w11wrrrr1rrr1yggyggyggaaaaaaaaayy1yy1yy1wwgwwgwwg'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube must have each middle face with a different color')
+        
